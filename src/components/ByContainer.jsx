2acb3fa4cc204e7e414e402e3aff19bccca66176
Mateus <atrujillomateus@gmail.com>
2024-05-20 00:31:55 -0500

    Forgot to make containers scrollable.

--- src/components/ByContainer.jsx
+++ src/components/ByContainer.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { ImageBackground, View } from "react-native";
+import { ImageBackground, ScrollView, View } from "react-native";
 import { tw } from "../libs";
 import { IMG_TRANSPARENT } from "../resources";
-import { ACCENT, TRANSPARENT, WHITE } from "./colors";
+import { LIGHT, TRANSPARENT, WHITE } from "./colors";
 import { CONTAINER_PADDING } from "./values";
 
 export const ByContainer = ({ children, bgImg, bgColor = WHITE, isRounded = false, twStyles = "" }) => {
+	const Children = () => <ScrollView style={tw`flex-1 bg-[${TRANSPARENT}]`}>{children}</ScrollView>;
 	const Container = () =>
 		bgImg ? (
 			<ImageBackground source={bgImg} defaultSource={IMG_TRANSPARENT} style={tw`flex-1 m-0 p-${CONTAINER_PADDING} ${twStyles}`}>
-				{children}
+				<Children />
 			</ImageBackground>
 		) : (
-			<View style={tw`flex-1 m-0 p-${CONTAINER_PADDING} bg-[${bgColor}] ${isRounded ? `rounded-t-${CONTAINER_PADDING * 2}` : ""} ${twStyles}`}>{children}</View>
+			<View style={tw`flex-1 m-0 p-${CONTAINER_PADDING} bg-[${bgColor}] ${isRounded ? `rounded-t-${CONTAINER_PADDING * 2}` : ""} ${twStyles}`}>
+				<Children />
+			</View>
 		);
 	return (
-		<View style={tw`flex-1 bg-[${isRounded ? ACCENT : TRANSPARENT}]`}>
+		<View style={tw`flex-1 bg-[${isRounded ? LIGHT : TRANSPARENT}]`}>
 			<Container />
 		</View>
 	);
